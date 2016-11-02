934edd17371edfe66debed7dc2457779cdf85937
RunnyYolk <nickturner57@gmail.com>
2016-11-02 19:54:54 +0000

    enquiry form working, success flash notification working.

---
+++ public/javascript/master.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  var btns = $(".enquire-btn");
+  btns.click(function(){
+    $(".enquiries").toggleClass("open");
+    $(".nav-right").toggleClass("enquiries-open");
+  })
+
+  $(".down-arrow").click(function(){
+    var offset = $(".section-two").offset();
+    $("html, body").animate({
+      scrollTop: offset.top
+    }, 700, "swing");
+  });
+
+  $('.enquiries-form').on('submit', function (e){
+    e.preventDefault()
+    var that = $(this);
+    var ajaxData = {};
+
+    that.find('[name]').each(function(i, field){
+      let name = $(field).attr('name'),
+          value = $(field).val();
+      ajaxData[name] = value
+    });
+
+    $.ajax({
+          url: "/enquiry",
+          type: "post",
+          data: ajaxData,
+          // dataType: 'json',
+          // cache: false,
+          // contentType: 'application/json',
+          // processData: false,
+          success: function(data) {
+            console.log('success');
+            console.log(data);
+          },
+          error: function(xhr, textStatus, errorThrown) {
+            console.log(xhr);
+            console.log(textStatus);
+            console.log(errorThrown);
+          }
+        });
+
+  })
+
+});
